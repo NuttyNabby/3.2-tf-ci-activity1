e3f39209b7107841335fd7c4dacb2a37acb284eb
NuttyNabby <nabilah.tang@gmail.com>
2025-06-06 16:02:32 +0800

    fixing bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce8-tfstate"
+    bucket = "sctp-ce9-tfstate"
     key    = "nabilah-s3-tf-ci.tfstate" #Change this
     region = "ap-southeast-1"
   }
