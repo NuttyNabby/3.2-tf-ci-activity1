c7b14b59178625f369f20ff8d7b179905cf184cf
NuttyNabby <nabilah.tang@gmail.com>
2025-06-06 18:17:48 +0800

    added other params for checkov

--- main.tf
+++ main.tf
@@ -29,3 +29,52 @@ locals {
 resource "aws_s3_bucket" "s3_tf" {
   bucket = format("%s-s3-tf-bkt-%s", local.name_prefix, local.account_id)
 }
+
+resource "aws_s3_bucket_versioning" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+  target_bucket = "nabilah-logging-bucket"
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_kms_key" "s3_key" {
+  description = "KMS key for S3 encryption"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "s3_tf" {
+  bucket = aws_s3_bucket.s3_tf.id
+
+  rule {
+    id     = "expire-old-objects"
+    status = "Enabled"
+
+    expiration {
+      days = 365
+    }
+  }
+}
